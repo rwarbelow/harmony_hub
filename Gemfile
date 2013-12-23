0234ee02b21c662fabcc1bdcba71fd6fa556b026
Rachel Warbelow <rachelwarbelow@gmail.com>
2013-12-22 19:50:52 -0800

    facebook omniauth

--- Gemfile
+++ Gemfile
@@ -22,6 +22,8 @@ gem 'coffee-rails', '~> 4.0.0'
 gem 'jquery-rails'
 gem 'devise'
 gem 'omniauth'
+gem 'omniauth-facebook' 
+gem 'activerecord-session_store'
 
 # Turbolinks makes following links in your web application faster. Read more: https://github.com/rails/turbolinks
 gem 'turbolinks'
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,5 +2,39 @@ class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable, :omniauthable
+         :recoverable, :rememberable, :trackable, 
+         :validatable, :omniauthable, :omniauth_providers => [:facebook]
+
+	def self.find_for_facebook_oauth(auth, signed_in_resource=nil)
+	  user = User.where(:provider => auth.provider, :uid => auth.uid).first
+	  unless user
+	    user = User.create(name:auth.extra.raw_info.name,
+	                         provider:auth.provider,
+	                         uid:auth.uid,
+	                         email:auth.info.email,
+	                         password:Devise.friendly_token[0,20]
+	                         )
+	  end
+	  user
+	end
+
+	def self.new_with_session(params, session)
+    super.tap do |user|
+      if email = session["facebook_email"]
+        user.email = email if user.email.blank?
+      end
+    end
+  end
+
+  private 
+
+  def user_params
+    params.require(:user).permit(
+    :provider, :uid, :name, :email, 
+    :encrypted_password, :reset_password_token, 
+    :reset_password_sent_at, :remember_created_at, 
+    :sign_in_count, :current_sign_in_at, :last_sign_in_at, 
+    :current_sign_in_ip, :last_sign_in_ip)
+  end
+
 end
